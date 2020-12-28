edc6bce44acda363c9b6e5683d730e367d9ed639
mahoshojohcg <yzy688@gmail.com>
2020-12-28 20:26:00 +0800

    Add files.

---
+++ demo/DistributionCheckerDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HCGStudio.DistributionChecker;
+
+namespace DistributionCheckerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OperatingSystem.IsLinux()
+                ? $"Your distribution info : {new DistributionChecker().GetDistribution()}"
+                : "You are not running in linux, try in Linux to get the information!");
+        }
+    }
+}
