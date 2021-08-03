6ed5f5042f5cd925c7d014bebcbb93d23a0c287f
牧瀬紅莉栖 <MakiseKurisu@users.noreply.github.com>
2021-08-03 13:48:23 +0800

    Add sample code

--- demo/DistributionCheckerDemo/Program.cs
+++ demo/DistributionCheckerDemo/Program.cs
@@ -5,11 +5,26 @@ namespace DistributionCheckerDemo
 {
     class Program
     {
+        static string sampleOsRelease = 
+@"NAME=""DistributionChecker Linux""
+ID=DistributionChecker
+ID_LIKE=arch
+BUILD_ID=rolling
+PRETTY_NAME=""DistributionChecker Linux""
+ANSI_COLOR=""1;2;3;4;5""
+HOME_URL=""https://DistributionChecker.org/""
+DOCUMENTATION_URL=""https://wiki.DistributionChecker.org/""
+SUPPORT_URL=""https://DistributionChecker.org/""
+BUG_REPORT_URL=""https://bugs.DistributionChecker.org/""
+LOGO=DistributionCheckerLinux
+";
+
         static void Main(string[] args)
         {
             Console.WriteLine(OperatingSystem.IsLinux()
                 ? $"Your distribution info : {new DistributionChecker().GetDistribution()}"
                 : "You are not running linux, try on Linux to get the information!");
+            Console.WriteLine($"Your sample distribution info : {new DistributionChecker(sampleOsRelease).GetDistribution()}");
         }
     }
 }
